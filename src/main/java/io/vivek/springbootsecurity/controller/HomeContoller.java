04960b8f63376059083de1b7e64a5d79833d6594
Vivekanand <vivek.prasad7@gmail.com>
2020-05-28 23:24:16 +0530

    Response status 201 Created and get location: URI

--- src/main/java/io/vivek/springbootsecurity/controller/HomeContoller.java
+++ src/main/java/io/vivek/springbootsecurity/controller/HomeContoller.java
@@ -1,7 +1,15 @@
 package io.vivek.springbootsecurity.controller;
 
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import io.vivek.springbootsecurity.model.User;
 
 @RestController
 public class HomeContoller {
@@ -19,6 +27,16 @@ public class HomeContoller {
 	@GetMapping("/admin")
 	public String getAdmin() {
 		return "<h1>Hello Admin</h1>";
+	}
+	
+	@PostMapping("/user")
+	public ResponseEntity<Object> saveUser(@RequestBody User user) {
+		//User savedUser = service.saveUser(user);
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(user.getUsername()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
---
+++ src/main/java/io/vivek/springbootsecurity/model/User.java
@@ -0,0 +1,33 @@
+package io.vivek.springbootsecurity.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	private String username;
+	private String password;
+	private Boolean enabled;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Boolean getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	
+}
